c64b92bc5c3e391f8495c5c3575a65f6ba45d06d
Jú Gabriele <127549501+Juli-Gabriele@users.noreply.github.com>
2026-04-21 14:36:33 -0300

    Atualizando Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import src.EscalonadorSRT;
 import src.Menu;
 
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     EscalonadorSRT escalonador = new EscalonadorSRT();
 
